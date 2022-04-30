54637140bb4e8758f372e9a0cdabca73f94b1860
Christian-210 <workwithcaw@gmail.com>
2022-04-30 15:59:47 -0500

    Added the UI Styles Credit to the author of the images: <a href="https://www.vecteezy.com/free-vector/rock-paper-scissors">Rock Paper Scissors Vectors by Vecteezy</a>

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
-let userPlay = prompt(
-  "Please make a selection. Select rock, paper, or scissors."
-).toLowerCase();
+// let userPlay = prompt(
+//   "Please make a selection. Select rock, paper, or scissors."
+// ).toLowerCase();
+
+// const rockBtn = document.getElementById("btn-rock");
+// const paperBtn = document.getElementById("btn-paper");
+// const scissorsBtn = document.getElementById("btn-scissors");
+
+const playerBtns = document.querySelectorAll("button");
+const p1Score = document.getElementById("player-score");
+const p2Score = document.getElementById("cpu-score");
+const gameMsg = document.querySelector(".win-lose-msg");
+let playerChoice;
 
 let playerScore = 0;
 let cpuScore = 0;
+let winningScore = 5;
 
 function computerPlay() {
   const characters = ["rock", "paper", "scissors"];
@@ -17,14 +28,18 @@ function computerPlay() {
 }
 
 function playRound(playerSelection, computerSelection) {
+  console.log(playerSelection, computerSelection);
+
   if (playerSelection === "rock") {
     if (playerSelection === computerSelection) {
       return `It's a tie!`;
     } else if (computerSelection === "scissors") {
       playerScore++;
+      p1Score.textContent = playerScore;
       return `You win! ${playerSelection} beats ${computerSelection}.`;
     } else {
       cpuScore++;
+      p2Score.textContent = cpuScore;
       return `You lose! ${computerSelection} beats ${playerSelection}.`;
     }
   }
@@ -34,9 +49,11 @@ function playRound(playerSelection, computerSelection) {
       return `It's a tie!`;
     } else if (computerSelection === "paper") {
       playerScore++;
+      p1Score.textContent = playerScore;
       return `You win! ${playerSelection} beats ${computerSelection}.`;
     } else {
       cpuScore++;
+      p2Score.textContent = cpuScore;
       return `You lose! ${computerSelection} beats ${playerSelection}.`;
     }
   }
@@ -46,39 +63,59 @@ function playRound(playerSelection, computerSelection) {
       return `It's a tie!`;
     } else if (computerSelection === "rock") {
       playerScore++;
+      p1Score.textContent = playerScore;
       return `You win! ${playerSelection} beats ${computerSelection}.`;
     } else {
       cpuScore++;
+      p2Score.textContent = cpuScore;
       return `You lose! ${computerSelection} beats ${playerSelection}.`;
     }
   }
 }
 
-function game() {
-  let rounds = 5;
+playerBtns.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    playerChoice = btn.value;
 
-  for (let i = 0; i < rounds; i++) {
-    if (i === 0) {
-      console.log(playRound(userPlay, computerPlay()));
-    } else {
-      userPlay = prompt(
-        "Please make a selection. Select rock, paper, or scissors."
-      ).toLowerCase();
+    if (playerScore !== winningScore && cpuScore !== winningScore) {
+      playRound(playerChoice, computerPlay());
 
-      console.log(playRound(userPlay, computerPlay()));
+      if (playerScore === 5) {
+        playerBtns.forEach((btn) => btn.setAttribute("disabled", ""));
+        gameMsg.textContent = "Congrats! You destroyed your opponent!";
+      }
+
+      if (cpuScore === 5) {
+        playerBtns.forEach((btn) => btn.setAttribute("disabled", ""));
+        gameMsg.textContent =
+          "Game over! You have fallen victim to your opponent!";
+      }
     }
-  }
+  });
+});
 
-  if (playerScore > cpuScore) {
-    console.log(`Congrats! You Won! Player:${playerScore} CPU:${cpuScore}`);
-  } else if (playerScore === cpuScore) {
-    console.log(`Oh darn! It's a tie! Player:${playerScore} CPU:${cpuScore}`);
-  } else {
-    console.log(`Sorry you lost! Player:${playerScore} CPU:${cpuScore}`);
-  }
+// function game() {
+//   let rounds = 5;
 
-  //   console.log(`Player:${playerScore} CPU:${cpuScore}`);
-}
+//   for (let i = 0; i < rounds; i++) {
+//     if (i === 0) {
+//       console.log(playRound(userPlay, computerPlay()));
+//     } else {
+//       userPlay = prompt(
+//         "Please make a selection. Select rock, paper, or scissors."
+//       ).toLowerCase();
 
-// console.log(playGame(userPlay, computerPlay()));
-game();
+//       console.log(playRound(userPlay, computerPlay()));
+//     }
+//   }
+
+//   if (playerScore > cpuScore) {
+//     console.log(`Congrats! You Won! Player:${playerScore} CPU:${cpuScore}`);
+//   } else if (playerScore === cpuScore) {
+//     console.log(`Oh darn! It's a tie! Player:${playerScore} CPU:${cpuScore}`);
+//   } else {
+//     console.log(`Sorry you lost! Player:${playerScore} CPU:${cpuScore}`);
+//   }
+// }
+
+// game();
